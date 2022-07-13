c256d221b04de7810f8f0b259af668986e11ff76
Sparsh Goyal <sparsh.goyal@ukg.com>
2022-07-14 02:39:55 +0530

    First Commit

--- EmployeeApplication/Program.cs
+++ EmployeeApplication/Program.cs
@@ -13,21 +13,21 @@ namespace EmployeeApplication
     {
         static void Main(string[] args)
         {
-            //EmployeeLogic employeeLogic = new EmployeeLogic();
+            EmployeeLogic employeeLogic = new EmployeeLogic();
 
-            //employeeLogic.AddEmployee(new Employee(1000, "James", "Software engineering", 80000));
-            //employeeLogic.AddEmployee(new Employee(2000, "Tim", "Quality Assuarance", 70000));
-            //employeeLogic.AddEmployee(new Employee(3000, "Bob", "Human Resource Dept", 75000));
-            //employeeLogic.AddEmployee(new Employee(4000, "Mike", "Accounting", 80000));
-            //employeeLogic.AddEmployee(new Employee(5000, "Frank", "R n D Dept", 90000));
+            employeeLogic.AddEmployee(new Employee(1000, "James", "Software engineering", 80000));
+            employeeLogic.AddEmployee(new Employee(2000, "Tim", "Quality Assuarance", 70000));
+            employeeLogic.AddEmployee(new Employee(3000, "Bob", "Human Resource Dept", 75000));
+            employeeLogic.AddEmployee(new Employee(4000, "Mike", "Accounting", 80000));
+            employeeLogic.AddEmployee(new Employee(5000, "Frank", "R n D Dept", 90000));
 
 
-            //List<Employee> list = employeeLogic.GetEmployees();
+            List<Employee> list = employeeLogic.GetEmployees();
 
-            //foreach(Employee e in list)
-            //{
-            //    e.displayEmployeeDetails();
-            //}
+            foreach (Employee e in list)
+            {
+                e.displayEmployeeDetails();
+            }
 
 
             Employee employee = new Manager(1000, "James", "Software Engineer", 400000);
